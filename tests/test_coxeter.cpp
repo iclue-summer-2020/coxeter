4d8f2ab271387db6550880a6b9bb64b6b2118477
David Brewster <davidb2@illinois.edu>
2021-10-03 11:51:34 -0500

    fix demo

--- tests/test_coxeter.cpp
+++ tests/test_coxeter.cpp
@@ -1,17 +1,18 @@
 // Copyright (c) 2020 [Your Name]. All rights reserved.
 
 #include <iostream>
+#include <memory>
 
 #include <coxeter/fcoxgroup.h>
+#include <coxeter/interactive.h>
 #include <coxeter/constants.h>
+
 
 int main() {
     // Always need to run this line first.
     constants::initConstants();
 
-    fcoxgroup::FiniteCoxGroup group("A", 5);
+    const std::unique_ptr<interactive::CoxGroup> group(interactive::coxeterGroup("A", 5));
 
-    std::cout << group.order() << std::endl;
-
-    // Segfaults here for some reason?
+    std::cout << group->order() << std::endl;
 }
